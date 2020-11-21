9c2bd5e1233c5d659432a651759b085ac2216083
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2020-11-21 11:53:02 +0100

    added content in Sell & Rent pages

--- pages/rent/index.jsx
+++ pages/rent/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import GeneralLayout from '../../components/Layout/GeneralLayout'
+import { Banner } from "./styled"
 
 const Rent = () => {
   return (
     <GeneralLayout>
-     <h1>Rent</h1>
+     <Banner>
+        <div className="overlay">
+          <section>
+            <div className="info">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                accumsan vel odio sit amet lacinia. Integer placerat, diam ut
+                sagittis tincidunt, turpis justo viverra metus, vel finibus mi
+                tortor eu purus.
+              </p>
+            </div>
+          </section>
+        </div>
+        </Banner>
     </GeneralLayout>
   )
 }
