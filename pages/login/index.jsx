521a78abd03eb73a199d54fee262464bd1530159
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2020-11-24 13:00:16 +0100

    modified login page

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,25 +1,30 @@
-import React from "react";
-import AuthComponent from "../../components/Auth";
-import { CurrentIcon, ChatIcon } from "../../assets/svg";
-import LoginForm from '../../screens/AuthPages/Login';
-import withApollo from "../../lib/apollo/withApollo";
+// import React from "react";
+// import AuthComponent from "../../components/Auth";
+// import { CurrentIcon, ChatIcon } from "../../assets/svg";
+// import LoginForm from '../../screens/AuthPages/Login';
+// import withApollo from "../../lib/apollo/withApollo";
 
-const Login = () => {
-	const tab = [
-		{
-			id: 1,
-			icon: <CurrentIcon height="12px" width="12px" color="#6c006c" />,
-			title: "User",
-			bodyText: "Login to start making our leaders accountable",
-			content: <LoginForm type={1} user={"user"} />,
-		},
-	];
+// const Login = () => {
+// 	const tab = [
+// 		{
+// 			id: 1,
+// 			icon: <CurrentIcon height="12px" width="12px" color="#6c006c" />,
+// 			title: "User",
+// 			bodyText: "Login to start making our leaders accountable",
+// 			content: <LoginForm type={1} user={"user"} />,
+// 		},
+// 	];
 
-	return (
-		<div>
-			<AuthComponent tab={tab} />
-		</div>
-	);
-};
+// 	return (
+// 		<div>
+// 			<AuthComponent tab={tab} />
+// 		</div>
+// 	);
+// };
 
-export default withApollo(Login);
+// export default withApollo(Login);
+
+import React from 'react'
+
+const Login = () => <div>Juliet</div>
+export default Login
